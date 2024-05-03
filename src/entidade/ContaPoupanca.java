2a85736355aee89d9e1ceed52105dcc4e8983c53
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-03 18:36:43 -0300

    conta poupanca adicionado

---
+++ src/entidade/ContaPoupanca.java
@@ -0,0 +1,12 @@
+package entidade;
+
+public class ContaPoupanca extends ContaAbstrata{
+
+    public ContaPoupanca(int numero, Cliente cliente) {
+        super(numero, cliente);
+    }
+    
+    public ContaPoupanca(int numero, double saldo, Cliente cliente) {
+        super(numero,saldo,cliente);
+    }
+}
